cac7bb7c865162b4f1157d9960f500624634dcfd
hong <devopscow@yahoo.com>
2024-03-17 22:11:13 -0500

    add docker

--- jenkins-user-data.sh
+++ jenkins-user-data.sh
@@ -5,6 +5,10 @@ export HOME=/home/ubuntu # not set on default
 sudo apt-get update
 sudo apt-get install -y fontconfig openjdk-17-jre
 
+# Node JS
+sudo apt update
+sudo apt install nodejs npm
+
 # jenkins
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
     https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
@@ -13,4 +17,24 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
     /etc/apt/sources.list.d/jenkins.list >/dev/null
 sudo apt-get update
 sudo apt-get install -y jenkins
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl
+sudo install -y -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+    sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo usermod -aG docker jenkins
+
+# Start Jenkins
 sudo systemctl start jenkins
